5f5ebfe871d2720deef4e25458b01bc742f02d0f
AMEENA SHAHEEN <ameena.sh.98@gmail.com>
2021-08-07 11:12:56 +0300

    admin route slash

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\admin\CategoryController;
 
+
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use App\Http\Controllers\HomeController;
 
 
 Route::group(
@@ -13,10 +15,11 @@ Route::group(
         'prefix' => LaravelLocalization::setLocale(),
         'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
     ], function(){
+        Route::get('/' , [HomeController::class , 'index'])->name('dashboard');
+
         Auth::routes();
         Auth::routes(['register' => false]);
 
-        Route::get('/' , [DashboardController::class , 'index'])->name('dashboard');
 
         Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/dashboard' , [DashboardController::class , 'index'])->name('dashboard');
