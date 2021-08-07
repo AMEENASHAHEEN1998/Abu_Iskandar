c7caa48fb9856a90cf7355375d14323e7811ddf6
AMEENA SHAHEEN <ameena.sh.98@gmail.com>
2021-08-07 12:40:44 +0300

    sidebar icon

--- resources/views/admin/layouts/main-sidebar.blade.php
+++ resources/views/admin/layouts/main-sidebar.blade.php
@@ -20,78 +20,60 @@
                     <li>
 
                         <a href="javascript:void(0);" data-toggle="collapse" data-target="#elements">
-                            <div class="pull-left"><i class="fa fa-newspaper-o" aria-hidden="true"></i><span
+                            <div class="pull-left"><i class="fa fa-diamond" aria-hidden="true"></i><span
                                     class="right-nav-text">{{ trans('admin/dashboard.category') }}</span></div>
                             <div class="pull-right"><i class="ti-plus"></i></div>
                             <div class="clearfix"></div>
                         </a>
                         <ul id="elements" class="collapse" data-parent="#sidebarnav">
 
-                            <li><a href="{{ route('admin.categories.index') }}">{{ trans('admin/dashboard.all_category') }}</a></li>
+                            <li><a href="{{ route('admin.categories.index') }}">{{ trans('admin/dashboard.primary_category') }}</a></li>
 
 
-                            <li><a href="">{{ trans('admin/dashboard.dashboard') }}</a></li>
-
-
-                            <li><a href="">{{ trans('admin/dashboard.dashboard') }}</a></li>
-
+                            <li><a href="">{{ trans('admin/dashboard.sub_category') }}</a></li>
 
                         </ul>
                     </li>
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#calendar-menu">
+                            <div class="pull-left"><i class="fa fa-cubes" aria-hidden="true"></i><span
+                                    class="right-nav-text">{{ trans('admin/dashboard.product') }}</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
 
-                        <!-- menu title -->
-                        <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title"></li>
-                        <!-- menu item calendar-->
-                        <li>
+                        <ul id="calendar-menu" class="collapse" data-parent="#sidebarnav">
+                            <li> <a href="">{{ trans('admin/dashboard.add_product') }} </a> </li>
 
-                            <a href="javascript:void(0);" data-toggle="collapse" data-target="#calendar-menu">
-                                <div class="pull-left"><i class="ti-calendar"></i><span
-                                        class="right-nav-text">{{ trans('admin/dashboard.dashboard') }}</span></div>
-                                <div class="pull-right"><i class="ti-plus"></i></div>
-                                <div class="clearfix"></div>
-                            </a>
+                                <li> <a href=""> {{ trans('admin/dashboard.show_products') }}</a> </li>
 
-                            <ul id="calendar-menu" class="collapse" data-parent="#sidebarnav">
-                                <li> <a href="">{{ trans('admin/dashboard.dashboard') }} </a> </li>
-
-                                    <li> <a href=""> {{ trans('admin/dashboard.dashboard') }}</a> </li>
-
-                            </ul>
+                        </ul>
 
 
                         </li>
-
-                        <!-- menu title -->
-                        <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title"> {{ trans('admin/dashboard.dashboard') }}</li>
-                        <!-- menu item calendar-->
                         <li>
-
                             <a href="javascript:void(0);" data-toggle="collapse" data-target="#join_reporters">
                                 <div class="pull-left"><i class="fa fa-paper-plane-o" aria-hidden="true"></i><span
-                                        class="right-nav-text">{{ trans('admin/dashboard.dashboard') }}</span></div>
+                                        class="right-nav-text">{{ trans('admin/dashboard.orders') }}</span></div>
                                 <div class="pull-right"><i class="ti-plus"></i></div>
                                 <div class="clearfix"></div>
                             </a>
 
 
-                        </li>
+
 
 
                             <ul id="join_reporters" class="collapse" data-parent="#sidebarnav">
-                                <li> <a href=""> {{ trans('admin/dashboard.dashboard') }}</a> </li>
+                                <li> <a href=""> {{ trans('admin/dashboard.add_order') }}</a> </li>
+                                <li> <a href=""> {{ trans('admin/dashboard.pending_orders') }}</a> </li>
+                                <li> <a href=""> {{ trans('admin/dashboard.orders_delivered') }}</a> </li>
 
                             </ul>
-
-
-                    <!-- menu title -->
-                    <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title"> {{ trans('admin/dashboard.dashboard') }}</li>
-
-
+                        </li>
                     <li>
-
                         <a href="javascript:void(0);" data-toggle="collapse" data-target="#elementsuser">
-                            <div class="pull-left"><i class="fa fa-users" aria-hidden="true"></i><span
-                                    class="right-nav-text">{{ trans('admin/dashboard.dashboard') }}</span></div>
+                            <div class="pull-left"><i class="fa fa-gift" aria-hidden="true"></i><span
+                                    class="right-nav-text">{{ trans('admin/dashboard.offers') }}</span></div>
                             <div class="pull-right"><i class="ti-plus"></i></div>
                             <div class="clearfix"></div>
                         </a>
@@ -99,42 +81,113 @@
 
                         <ul id="elementsuser" class="collapse" data-parent="#sidebarnav">
 
-                            <li><a  href="">{{ trans('admin.dashboard.dashboard') }}</a></li>
+                            <li><a  href="">{{ trans('admin.dashboard.add_offer') }}</a></li>
 
 
-                            <li><a  href=""> {{ trans('admin/dashboard.dashboard') }}</a></li>
+                            <li><a  href=""> {{ trans('admin/dashboard.show_offers') }}</a></li>
+                            <li><a  href=""> {{ trans('admin/dashboard.old_offers') }}</a></li>
 
 
                         </ul>
                     </li>
 
-                    <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title"> </li>
 
                     <li>
                         <a href="javascript:void(0);" data-toggle="collapse" data-target="#site">
-                            <div class="pull-left"><i class="fa fa-globe" aria-hidden="true"></i><span
-                                    class="right-nav-text">{{ trans('admin/dashboard.dashboard') }}</span></div>
+                            <div class="pull-left"><i class="fa fa-pencil-square-o" aria-hidden="true"></i><span
+                                    class="right-nav-text">{{ trans('admin/dashboard.jobs') }}</span></div>
                             <div class="pull-right"><i class="ti-plus"></i></div>
                             <div class="clearfix"></div>
                         </a>
                         <ul id="site" class="collapse" data-parent="#sidebarnav">
-                            <li><a  href="">{{ trans('admin/dashboard.dashboard') }}</a></li>
+                            <li><a  href="">{{ trans('admin/dashboard.add_job') }}</a></li>
+                            <li><a  href="">{{ trans('admin/dashboard.show_job') }}</a></li>
+                            <li><a  href="">{{ trans('admin/dashboard.old_job') }}</a></li>
 
                         </ul>
                     </li>
-                    <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title"> </li>
+
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#site">
+                            <div class="pull-left"><i class="fa fa-paper-plane-o" aria-hidden="true"></i><span
+                                    class="right-nav-text">{{ trans('admin/dashboard.request_job') }}</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="site" class="collapse" data-parent="#sidebarnav">
+                            <li><a  href="">{{ trans('admin/dashboard.show_request_job') }}</a></li>
+                            <li><a  href="">{{ trans('admin/dashboard.accepted_request') }}</a></li>
+                            <li><a  href="">{{ trans('admin/dashboard.rejected_request') }}</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#site">
+                            <div class="pull-left"><i class="fa fa-file-text-o" aria-hidden="true"></i><span
+                                    class="right-nav-text">{{ trans('admin/dashboard.articles') }}</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="site" class="collapse" data-parent="#sidebarnav">
+                            <li><a  href="">{{ trans('admin/dashboard.add_articles') }}</a></li>
+                            <li><a  href="">{{ trans('admin/dashboard.show_articles') }}</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#site">
+                            <div class="pull-left"><i class="fa fa-users" aria-hidden="true"></i><span
+                                    class="right-nav-text">{{ trans('admin/dashboard.distributors') }}</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="site" class="collapse" data-parent="#sidebarnav">
+                            <li><a  href="">{{ trans('admin/dashboard.distributors') }}</a></li>
+                            <li><a  href="">{{ trans('admin/dashboard.category_distributors') }}</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#site">
+                            <div class="pull-left"><i class="fa fa-users" aria-hidden="true"></i><span
+                                    class="right-nav-text">{{ trans('admin/dashboard.staff') }}</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="site" class="collapse" data-parent="#sidebarnav">
+                            <li><a  href="">{{ trans('admin/dashboard.add_member') }}</a></li>
+                            <li><a  href="">{{ trans('admin/dashboard.show_members') }}</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#site">
+                            <div class="pull-left"><i class="fa fa-info" aria-hidden="true"></i><span
+                                    class="right-nav-text">{{ trans('admin/dashboard.information_company') }}</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="site" class="collapse" data-parent="#sidebarnav">
+                            <li><a  href="">{{ trans('admin/dashboard.information_company') }}</a></li>
+
+                        </ul>
+                    </li>
 
                     <li>
                         <a href="javascript:void(0);" data-toggle="collapse" data-target="#logout">
                             <div class="pull-left"><i class=" ti-unlock"></i><span
-                                    class="right-nav-text">{{ trans('admin/dashboard.dashboard') }}</span></div>
+                                    class="right-nav-text">{{ trans('admin/dashboard.logout') }}</span></div>
                             <div class="pull-right"><i class="ti-plus"></i></div>
                             <div class="clearfix"></div>
                         </a>
                         <ul id="logout" class="collapse" data-parent="#sidebarnav">
                             <li><a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i
-                                     class="bx bx-log-out"></i>logout</a>
+                                     class="bx bx-log-out"></i>{{ trans('admin/dashboard.logout') }}</a>
                              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                  @csrf
                              </form></li>
