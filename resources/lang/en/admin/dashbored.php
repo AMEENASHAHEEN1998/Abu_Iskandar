a88c8a4057e0fa97d7c97d8ecd9400f692e13432
AMEENA SHAHEEN <ameena.sh.98@gmail.com>
2021-08-07 11:17:43 +0300

    sidebar lang

--- resources/lang/en/admin/dashbored.php
+++ resources/lang/en/admin/dashbored.php
@@ -3,5 +3,41 @@
 return [
 
 
+    // start sidebar
+    'dashboard' => 'Dashboard',
+    'site_name' => 'Abu Iskandar',
+    'category'  => 'Category',
+    'primary_category' => ' Primary Category ',
+    'sub_category' => 'Sub Category ',
+    'product' => 'Product',
+    'add_product' => 'Add Product ',
+    'show_products' => 'Show Product',
+    'orders' => 'Orders',
+    'add_order' => 'Add Order',
+    'pending_orders' => 'Pending Orders  ',
+    'orders_delivered' => 'Orders Delivered  ',
+    'offers' => 'Offers',
+    'add_offer' => 'Add Offer ',
+    'show_offers' => 'Show Offers ',
+    'old_offers' => 'old Offers ',
+    'jobs' => 'Jobs',
+    'add_job' => 'Add Job ',
+    'show_job' => 'Show Job ',
+    'old_job' =>'Old Job',
+    'request_job' => 'Request Job ',
+    'show_request_job' => 'Show request job  ',
+    'accepted_request' => 'Accepted Request ',
+    'rejected_request' => 'Rejected Request ',
+    'articles' => 'Articles' ,
+    'add_articles' => 'Add Articles ',
+    'show_articles' => 'Show Articles ',
+    'distributors' => 'Distributors' ,
+    'category_distributors' => ' Category Distributors',
+    'staff' => 'Staff ' ,
+    'add_member' => 'Add Member ',
+    'show_members' => 'Show Members  ',
+    'information_company' => 'Information Company  ',
+    'logout' => 'Logout ',
+    // end sidebar
 
 ];
